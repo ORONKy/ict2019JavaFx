7c2f9f10af5d35b5c477d64f1e4fcdfb6e23a1bc
alvaro <varotom4@gmail.com>
2021-03-26 21:15:23 +0100

    Updated Participant
    * new member newUser which is set when the user is not in db jet
    * new constructors for newUser
    * Javadoc

--- src/ch/bbzsogr/ict2019/model/Participant.java
+++ src/ch/bbzsogr/ict2019/model/Participant.java
@@ -6,13 +6,51 @@ public class Participant
 	private String name;
 	private boolean temporary;
 	private boolean team;
+	private boolean newUser;
 
+	/**
+	 * Creates an user who is NOT an new user
+	 * @param id id of the user
+	 * @param name name of the user
+	 * @param temporary true if this user is temporary
+	 * @param team true if this user is in a team
+	 */
 	public Participant ( int id, String name, boolean temporary, boolean team )
 	{
 		this.id = id;
 		this.name = name;
 		this.temporary = temporary;
 		this.team = team;
+		this.newUser = false;
+	}
+
+	/**
+	 * creates an user
+	 * @param id id of the user
+	 * @param name name of the user
+	 * @param temporary true if this user is temporary
+	 * @param team true if this user is in a team
+	 * @param newUser true if this user is an not existing user jet
+	 */
+	public Participant ( int id, String name, boolean temporary, boolean team, boolean newUser )
+	{
+		this.id = id;
+		this.name = name;
+		this.temporary = temporary;
+		this.team = team;
+		this.newUser = newUser;
+	}
+
+	/**
+	 * Creates an empty user
+	 * @param newUser if true this is an not existing user jet
+	 */
+	public Participant(boolean newUser){
+		this.newUser = newUser;
+		id = 0;
+		name = "";
+		temporary = false;
+		team = false;
 	}
 
 	public int getId ()
@@ -33,5 +71,9 @@ public class Participant
 	public boolean isTemporary ()
 	{
 		return temporary;
+	}
+
+	public boolean isNewUser(){
+		return newUser;
 	}
 }
